bdb7de360e65eb43c56537a2f24e02e31a8ea49b
Nelson Brito <ntbrito@gmail.com>
2018-02-07 10:49:54 +0000

    Credentials for each account
    Account names must be consistent with the names on the file 'mfa.json'

---
+++ open_session/aws.py
@@ -0,0 +1,13 @@
+AWS = {
+    # Credentials for the '<account_name01>' account.
+    "<account_name01>": {
+        "aws_access_key_id": "",
+        "aws_secret_access_key": "",
+    },
+
+    # Credentials for the '<account_name02>' account.
+    "<account_name02>": {
+        "aws_access_key_id": "",
+        "aws_secret_access_key": "",
+    },
+}
